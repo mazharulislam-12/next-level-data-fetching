6323e4655057d677cbc2b2d6cb8aebb176597dd9
Mazharul Islam <505mazharulislam@gmail.com>
2024-03-02 16:19:19 +0600

    data fetching app create

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div>
+      <h2>Welcome to next my next level data fetching module</h2>
+    </div>
+  );
+};
+
+export default Homepage;
